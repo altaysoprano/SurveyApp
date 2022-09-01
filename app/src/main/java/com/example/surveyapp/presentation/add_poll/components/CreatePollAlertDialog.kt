71935e3c6edceeb5dd06fb36ffd1a3ab7328f4b6
Hasan Cihad Altay <caltayzzz@gmail.com>
2022-09-01 17:01:48 +0300

    copy code icon done

--- app/src/main/java/com/example/surveyapp/presentation/add_poll/components/CreatePollAlertDialog.kt
+++ app/src/main/java/com/example/surveyapp/presentation/add_poll/components/CreatePollAlertDialog.kt
@@ -1,27 +1,34 @@
 package com.example.surveyapp.presentation.add_poll.components
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.surveyapp.R
 
 @Composable
 fun CreatePollAlertDialog(dialogState: Boolean, code: String, onDismiss: () -> Unit) {
 
+    val clipboardManager: ClipboardManager = LocalClipboardManager.current
+
     if (dialogState) {
         AlertDialog(
             onDismissRequest = { },
-            title = {
+            text = {
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 32.dp),
+                        .fillMaxWidth(),
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Text(
@@ -29,20 +36,26 @@ fun CreatePollAlertDialog(dialogState: Boolean, code: String, onDismiss: () -> U
                         fontSize = 24.sp
                     )
                 }
-            },
-            text = {
-                Column(
+                Spacer(modifier = Modifier.height(32.dp))
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 32.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                        .padding(top = 32.dp).background(Color.LightGray),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
                         text = code, fontSize = 32.sp,
-                        fontWeight = FontWeight.Bold, textAlign = TextAlign.Center,
-
-                        )
+                        fontWeight = FontWeight.Bold, textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Icon(
+                        painter = painterResource(R.drawable.ic_copy_24),
+                        contentDescription = "print",
+                        modifier = Modifier.clickable {
+                            clipboardManager.setText(AnnotatedString((code)))
+                        }
+                    )
                 }
             },
             confirmButton = {
